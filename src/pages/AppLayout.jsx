157b37eead10b374644e9a3f38ba04e9defd85a2
Milan-1986 <milan86pk@gmail.com>
2024-10-28 22:55:39 +0100

    edit AppLayout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,7 +1,7 @@
-import Sidebar from "../Components/SideBar";
 import Map from "../Components/Map";
 import styles from "./AppLayout.module.css";
 import User from "../Components/User";
+import Sidebar from "../Components/Sidebar";
 
 function AppLayout() {
   return (
